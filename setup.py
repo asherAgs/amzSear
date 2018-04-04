1b2d8c059bd0fd4361d4aea4f5f48c748ed462e9
Asher Silvers <ashersilvers@gmail.com>
2018-04-04 17:56:23 +1000

    Small changes for upload

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 setup(
   name = 'amzsear',
   packages = find_packages(),
-  version = '1.9.16',
+  version = '2.0.0',
   description = 'The unofficial Amazon search CLI & Python API',
   author = "Asher Silvers",
   author_email = "ashersilvers@gmail.com",
   license='MIT',
-  url = 'https://github.com/asherAgs/amzSear_staging', 
+  url = 'https://github.com/asherAgs/amzSear', 
   keywords = 'amazon search product products python',
   classifiers = [],
   entry_points={
